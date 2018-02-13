1d31fa7a0ff99e548603bc533c11c472d2b47aba
julievdh <julievdh@mit.edu>
2018-02-13 17:55:37 +0100

    Redo anova for glide data

--- GlideCd.R
+++ GlideCd.R
@@ -1,54 +1,18 @@
-library(R.matlab)
+
 library(agricolae)
 require(PMCMR)
-library(xlsx)
+library(readxl)
 
-setwd("~/Documents/MATLAB/DQ/DQ2013/Glides")
-data <- readMat('Data_ReadNew.mat')
-Cdmean <- data$Cd.mn
-CDAS <- data$CDAS
 setwd("~/Documents/R/DQ_R")
-newdata <- read.xlsx("drag_coefficients_table.xlsx", 2)  # read 2nd sheet
+data <- read_excel('Cd Calculations - with success rate.xlsx',3)
 
 # test for homogeneity of variance: Levene's test (better for non-normal data)
-bartlett.test(Cdmean ~ data$Tag) # Method 1
-bartlett.test(CDAS ~ data$Tag) # Method 2
-bartlett.test(t(data$dur) ~ data$Tag) # Duration
+bartlett.test(data$CD ~ data$Condition) # Method 1
+# bartlett.test(CDAS ~ data$Tag) # Method 2
+bartlett.test(data$Duration ~ data$Condition) # Duration
+bartlett.test(data$Uo ~ data$Condition) # initial velocity
 
-bartlett.test(CdCorr ~ Tag, data = newdata)
-
-# Since variance increases with tag condition, use nonparametric kruskal-wallis
-# to test for effect of tag
-kruskal.test(Cdmean ~ data$Tag) # Method 1
-kruskal.test(CDAS ~ data$Tag) # Method 2
-posthoc.kruskal.dunn.test(x = CDAS, g = data$Tag,p.adjust.method = "none") # post-hoc dunn test
-kruskal.test(t(data$dur)~ data$Tag) # Duration
-posthoc.kruskal.dunn.test(x = t(data$dur), g = data$Tag,p.adjust.method = "none")
-
-kruskal.test(CdCorr ~ Tag,data = newdata) # Newdata
-posthoc.kruskal.dunn.test(x = newdata$CdCorr, g = newdata$Tag, p.adjust.method = "none") # post-hoc dunn test
-
-
-
-
-
-# Any difference in initial velocity?
-kruskal.test(data$sspeed~ data$Tag)
-# Difference in final velocity?
-kruskal.test(data$espeed~ data$Tag)
-
-
-Tag <- as.factor(data$Tag)
-foldchange <- matrix(NA, nrow=1, ncol=3)
-foldchange[1] <- mean(Cdmean[which(Tag == 1)])/mean(Cdmean[which(Tag == 0)])
-foldchange[2] <- mean(Cdmean[which(Tag == 3)])/mean(Cdmean[which(Tag == 0)])
-foldchange[3] <- mean(Cdmean[which(Tag == 5)])/mean(Cdmean[which(Tag == 0)])
-foldchange[4] <- mean(CDAS[which(Tag == 1)])/mean(CDAS[which(Tag == 0)])
-foldchange[5] <- mean(CDAS[which(Tag == 3)])/mean(CDAS[which(Tag == 0)])
-foldchange[6] <- mean(CDAS[which(Tag == 5)])/mean(CDAS[which(Tag == 0)])
-
-plot(c(1,2,3),foldchange[4:6],col = 'blue')
-points(c(1,2,3),foldchange[1:3],col = 'black')
+# equal variance in all parameters in different conditions. 
 
 # Multiple comparison for CFD data
 # from GlideDataPlots.m
@@ -61,17 +25,22 @@ CFD_Cd_tag4 <- c(0.0186,0.0169,0.0162,0.0160,0.0157,0.0158)
 allvel <- as.factor(rep(vel,4))
 tag <- as.factor(c(0,0,0,0,0,0,1,1,1,1,1,1,2,2,2,2,2,2,4,4,4,4,4,4))
 allCFD <- c(CFD_Cd_notag,CFD_Cd_tag,CFD_Cd_tag2,CFD_Cd_tag4)
-# ANOVA with Tukey's HSD
+# ANOVA with Tukey's HSD on CFD data
 CFD.linear.model <- lm(allCFD ~ allvel+tag)
 CFD.aov <- anova(CFD.linear.model)
 
 tukdur <-TukeyHSD(aov(CFD.linear.model))
 
-## ANOVA FOR NEW DATA (don't have to use KW based on equal variance)
-tag <- as.factor(newdata$Tag)
-Ind <- as.factor(newdata$Ind)
-CdCorr <- newdata$CdCorr
+#####-----  ANOVA for experimental data: drag coefficient
+tag <- as.factor(data$Condition)
+Ind <- as.factor(data$Animal)
+CdCorr <- data$CD
 
 Cdlm <- lm(CdCorr ~ tag+Ind)
 anova(Cdlm)
-tukdur <-TukeyHSD(aov(Cdlm))
+tukCd <-TukeyHSD(aov(Cdlm))
+
+Durlm <- lm(data$Duration ~ tag+Ind) 
+anova(Durlm)
+Ulm <- lm(data$Uo ~ tag+Ind)
+anova(Ulm)
