e1f6ee641aaf767d9da39614a58a2ca6b4c12262
julievdh <julievdh@mit.edu>
2018-02-13 18:15:37 +0100

    Redone for Submission

--- GlideCd.R
+++ GlideCd.R
@@ -1,6 +1,5 @@
 
 library(agricolae)
-require(PMCMR)
 library(readxl)
 
 setwd("~/Documents/R/DQ_R")
@@ -13,23 +12,6 @@ bartlett.test(data$Duration ~ data$Condition) # Duration
 bartlett.test(data$Uo ~ data$Condition) # initial velocity
 
 # equal variance in all parameters in different conditions. 
-
-# Multiple comparison for CFD data
-# from GlideDataPlots.m
-vel <- c(1,2,3,4,5,6)
-CFD_Cd_notag <- c(0.0127,0.0106,0.0097,0.0094,0.0093,0.0092)
-CFD_Cd_tag <- c(0.0132,0.0111,0.0102,0.0099,0.0098,0.0097)
-CFD_Cd_tag2 <- c(0.0159,0.0144,0.0132,0.0129,0.0128,0.0127)
-CFD_Cd_tag4 <- c(0.0186,0.0169,0.0162,0.0160,0.0157,0.0158)
-
-allvel <- as.factor(rep(vel,4))
-tag <- as.factor(c(0,0,0,0,0,0,1,1,1,1,1,1,2,2,2,2,2,2,4,4,4,4,4,4))
-allCFD <- c(CFD_Cd_notag,CFD_Cd_tag,CFD_Cd_tag2,CFD_Cd_tag4)
-# ANOVA with Tukey's HSD on CFD data
-CFD.linear.model <- lm(allCFD ~ allvel+tag)
-CFD.aov <- anova(CFD.linear.model)
-
-tukdur <-TukeyHSD(aov(CFD.linear.model))
 
 #####-----  ANOVA for experimental data: drag coefficient
 tag <- as.factor(data$Condition)
@@ -44,3 +26,26 @@ Durlm <- lm(data$Duration ~ tag+Ind)
 anova(Durlm)
 Ulm <- lm(data$Uo ~ tag+Ind)
 anova(Ulm)
+
+# Multiple comparison for CFD data
+data <- read_excel('Cd Calculations - with success rate.xlsx',6)
+
+tag <- as.factor(data$Condition)
+
+# ANOVA with Tukey's HSD on CFD data
+CFD.linear.model <- lm(data$Cd ~ tag)
+CFD.aov <- anova(CFD.linear.model)
+
+tukdur <-TukeyHSD(aov(CFD.linear.model))
+
+#### -- Success rate
+data <- read_excel('Cd Calculations - with success rate.xlsx',7)
+
+ggplot(data, aes(factor(Condition),Rate, fill = Animal)) + 
+  theme_bw() +
+  geom_bar(stat = "identity", position = "dodge", colour = "black") +
+  scale_fill_manual(values = c("grey","white"),name = " ", breaks = c(1,2), labels = c("Hoku","Liho")) +
+  theme(legend.position=c(0.9, 0.9)) +
+  xlab(" ") +
+  ylab("Success Rate") +
+  theme(axis.text=element_text(size=12), axis.title=element_text(size=14))
